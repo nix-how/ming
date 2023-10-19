33628f88630c2519feb9981a818627f3a48537e3
matthewcroughan <matt@croughan.sh>
2023-10-19 18:35:16 +0100

    initialise mqtt configuration nodes with Nix

--- vm.nix
+++ vm.nix
@@ -20,25 +20,62 @@
   };
   systemd.services.node-red.preStart = let
     baseFlowsJsonFile = builtins.toFile "flows.json" baseFlowsJson;
-    baseFlowsJson = builtins.toJSON {
-      id = "32deae1cf6a499f8";
-      type = "influxdb";
-      hostname = "127.0.0.1";
-      port = "8086";
-      protocol = "http";
-      database = "database";
-      name = "ming";
-      usetls = "false";
-      tls = "";
-      influxdbVersion = "2.0";
-      url = "http://localhost:8086";
-      rejectUnauthorized = true;
-    };
+    baseFlowsJson = builtins.toJSON [
+      # mqtt configuration node
+      {
+        autoConnect = true;
+        autoUnsubscribe = true;
+        birthMsg = {};
+        birthPayload = "";
+        birthQos = "0";
+        birthRetain = "false";
+        birthTopic = "";
+        broker = "localhost";
+        cleansession = true;
+        clientid = "";
+        closeMsg = {};
+        closePayload = "";
+        closeQos = "0";
+        closeRetain = "false";
+        closeTopic = "";
+        id = "803964aa67051e90";
+        keepalive = "60";
+        name = "ming";
+        port = "1883";
+        protocolVersion = "5";
+        sessionExpiry = "";
+        type = "mqtt-broker";
+        userProps = "";
+        usetls = false;
+        willMsg = {};
+        willPayload = "";
+        willQos = "0";
+        willRetain = "false";
+        willTopic = "";
+      }
+      # influxdb configuration node
+      {
+        id = "32deae1cf6a499f8";
+        type = "influxdb";
+        hostname = "127.0.0.1";
+        port = "8086";
+        protocol = "http";
+        database = "database";
+        name = "ming";
+        usetls = "false";
+        tls = "";
+        influxdbVersion = "2.0";
+        url = "http://localhost:8086";
+        rejectUnauthorized = true;
+      }
+    ];
   in
   ''
     if [ ! -d "${config.services.node-red.userDir}/flows.json" ]
     then
-      ${pkgs.jq}/bin/jq -s '.' < ${baseFlowsJsonFile} > ${config.services.node-red.userDir}/flows.json
+      # this may not be needed as long as we compose the flow with [ {} ] in Nix
+      # ${pkgs.jq}/bin/jq -s '.' < ${baseFlowsJsonFile} > ${config.services.node-red.userDir}/flows.json
+      cp --no-preserve=mode ${baseFlowsJsonFile} ${config.services.node-red.userDir}/flows.json
     else
       ${pkgs.jq}/bin/jq --slurpfile new_values ${baseFlowsJsonFile} 'map(if .id == "32deae1cf6a499f8" then . += $new_values[0] else . end)' ${config.services.node-red.userDir}/flows.json
     fi
